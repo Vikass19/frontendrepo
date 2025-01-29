9de0248bc104a4d2dfbe9f74d6e5535038a7f10c
Vikas Bansode <131449554+Vikass19@users.noreply.github.com>
2025-01-29 22:10:11 +0530

    Update postdetail.jsx

--- src/componants/postdetail.jsx
+++ src/componants/postdetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import "./postdetail.css";
+import "./Postdetail.css";
 
 const PostDetail = () => {
   const { postId } = useParams(); // Get the post ID from the URL
